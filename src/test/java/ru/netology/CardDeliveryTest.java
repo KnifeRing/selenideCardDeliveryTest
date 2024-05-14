256bb5649ab04c1b915dfd8ebbf83d0e69de95b1
Alexander Khyrbu <alexander.khyrbu@list.ru>
2024-05-14 11:06:36 +0300

    change gradle code

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,4 +1,28 @@
 package ru.netology;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
 public class CardDeliveryTest {
+
+    public String setDate(int gap, String pattern) {
+        return LocalDate.now().plusDays(gap).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    @BeforeEach
+    void setUp() {
+        Configuration.headless = true;
+        open("http://localhost:9999");
+    }
+
 }
