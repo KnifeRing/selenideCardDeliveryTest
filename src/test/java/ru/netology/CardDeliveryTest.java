6fcc710c51d57819094fafe0366b090fc43c464a
Alexander Khyrbu <alexander.khyrbu@list.ru>
2024-05-20 02:56:14 +0300

    change test logic

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -9,7 +9,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.open;
 
 public class CardDeliveryTest {
 
